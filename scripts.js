e37617c084287f79689ec25d6052b459e4856a73
jolak5 <kayodej4@gmail.com>
2022-12-20 09:00:50 +0100

    Stop overflow in the body

--- scripts.js
+++ scripts.js
@@ -5,17 +5,20 @@ const modal = document.querySelector(".modal");
 const closeModal = document.querySelector(".close-modal");
 const overlay = document.querySelector(".overlay");
 const mobileLinks = document.querySelectorAll(".mobile-links");
+const body = document.querySelector("body");
 
 // function that opens the mobile menu
 const mobileMenu = function () {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
+  body.style.overflow = "hidden";
 };
 
 // function that closes the mobile menu
 const closeMobileMenu = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
+  body.style.overflow = "visible";
 };
 for (let i = 0; i < mobileLinks.length; i++) {
   mobileLinks[i].addEventListener("click", closeMobileMenu);
